f1b2bb526d6814842672a1c0bcd8d64e726a6b7d
lengsambath2002-lgtm <leng.sambath.2002@gmail.com>
2026-06-02 11:33:55 +0700

    first commit

---
+++ src/main/java/com/sambath/admincafe/AdminCafeApplication.java
@@ -0,0 +1,13 @@
+package com.sambath.admincafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminCafeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminCafeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sambath/admincafe/AdminCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sambath.admincafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminCafeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
